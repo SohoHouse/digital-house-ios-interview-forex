a4244903455e97bc470914b2e3cb33cdcc0f96d0
Brett Neumayer <bratt.neumayer@gmail.com>
2020-09-08 17:25:31 +0100

    Initial empty project with one view / view controller.

---
+++ forex/forex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  forex
+//
+//  Created by bn on 08/09/2020.
+//  Copyright © 2020 bn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
